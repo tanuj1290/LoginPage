843f51d13b6bee4b1ea4684c4afe5cf4297f558d
Tanuj Kumar <tanujagrawal1290@gmail.com>
2022-12-21 23:05:09 +0530

    changed commit

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -1,42 +1,119 @@
 package com.example.loginpage
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
+
+    private val PASSWORD_PATTERN: Pattern = Pattern.compile("^" +
+                "(?=.*[@#$%^&+=])" +  // at least 1 special character
+                "(?=\\S+$)" +  // no white spaces
+                ".{4,}" +  // at least 4 characters
+                "$"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var temp_for_buttton_enabled_id: Boolean = false
+        var temp_for_buttton_enabled_password: Boolean = false
 
         val newUser = findViewById<TextView>(R.id.new_user)
         val forgotPassword = findViewById<TextView>(R.id.forgot_password)
         val loggedin = findViewById(R.id.login_button) as Button
         val userIdEmailPhone = findViewById<TextInputEditText>(R.id.user_id)
         val userPassword = findViewById<TextInputEditText>(R.id.user_password)
+        val userEmailContainer = findViewById<TextInputLayout>(R.id.user_email_container)
+        val passwordContainer = findViewById<TextInputLayout>(R.id.password_container)
 
-        loggedin.setOnClickListener(){
-            var idEmailOrPhone: String = userIdEmailPhone.text.toString()
-            var inputPassword: String = userPassword.text.toString()
-            if (idEmailOrPhone == null || idEmailOrPhone.trim() == "" || inputPassword == null || inputPassword.trim() == ""){
-                if (idEmailOrPhone == null || idEmailOrPhone.trim() == ""){
-                    userIdEmailPhone.setError("Please enter a id")
-                }
-                if (inputPassword == null || inputPassword.trim() == ""){
-                    userPassword.setError("Please enter a password")
-                }
+
+        userIdEmailPhone.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
-            else
-                Toast.makeText(this,"You are now logged in",Toast.LENGTH_SHORT).show()
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (android.util.Patterns.EMAIL_ADDRESS.matcher(userIdEmailPhone.text.toString())
+                        .matches() || android.util.Patterns.PHONE.matcher(userIdEmailPhone.text.toString()).matches()
+                ) {
+                    userEmailContainer.helperText = null
+                    userEmailContainer.isHelperTextEnabled = false
+//                    loggedin.isEnabled = true
+                    temp_for_buttton_enabled_id = true
+                } else {
+                    userEmailContainer.helperText = "* Enter a valid ID"
+//                    loggedin.isEnabled = false
+                    temp_for_buttton_enabled_id = false
+                }
+                if (temp_for_buttton_enabled_id && temp_for_buttton_enabled_password)
+                    loggedin.isEnabled = true
+                else
+                    loggedin.isEnabled = false
+            }
+        })
+
+        userPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                var password: String = userPassword.text.toString()
+                if (password.isEmpty()){
+                    passwordContainer.helperText = "Enter a password"
+                    passwordContainer.isHelperTextEnabled = true
+                    temp_for_buttton_enabled_password = false
+                }else if(!PASSWORD_PATTERN.matcher(password).matches()){
+                    passwordContainer.helperText = "Password is too weak"
+                    passwordContainer.isHelperTextEnabled = true
+                    temp_for_buttton_enabled_password = false
+                }else{
+                    passwordContainer.helperText = null
+                    passwordContainer.isHelperTextEnabled = false
+                    temp_for_buttton_enabled_password = true
+                }
+//
+//                if (userPassword.text.toString().isNotEmpty()){
+//                    passwordContainer.helperText = null
+//                    passwordContainer.isHelperTextEnabled = true
+////                    loggedin.isEnabled = true
+//                    temp_for_buttton_enabled_password = true
+//                }
+//                else{
+//                    passwordContainer.helperText = "* Enter a password"
+////                    loggedin.isEnabled = false
+//                    temp_for_buttton_enabled_password = false
+//                }
+
+                if (temp_for_buttton_enabled_id && temp_for_buttton_enabled_password)
+                    loggedin.isEnabled = true
+                else
+                    loggedin.isEnabled = false
+
+            }
+        })
+        loggedin.setOnClickListener() {
+//            var idEmailOrPhone: String = userIdEmailPhone.text.toString()
+//            var inputPassword: String = userPassword.text.toString()
+                Toast.makeText(this, "You are now logged in", Toast.LENGTH_SHORT).show()
         }
 
-        newUser.setOnClickListener(){
-            intent = Intent(this,MainActivity2::class.java)
+        newUser.setOnClickListener() {
+            intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/loginpage/MainActivity2.kt
+++ app/src/main/java/com/example/loginpage/MainActivity2.kt
@@ -3,19 +3,60 @@ package com.example.loginpage
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         val signUpButtonClicked = findViewById<Button>(R.id.signup_button)
+        val namecontainer = findViewById<TextInputLayout>(R.id.name_container)
+        val emailcontainer = findViewById<TextInputLayout>(R.id.email_container)
+        val phonecontainer = findViewById<TextInputLayout>(R.id.phone_container)
+        val password1container = findViewById<TextInputLayout>(R.id.password_1_container)
+        val password2container = findViewById<TextInputLayout>(R.id.password_2_container)
 
-        signUpButtonClicked.setOnClickListener(){
-            Toast.makeText(this,"You are Successfully Signed Up",Toast.LENGTH_SHORT).show()
-            intent = Intent(this,MainActivity::class.java)
+        val nameEditTextSignup = findViewById<TextInputEditText>(R.id.name_editText)
+        val emailEditTextSignup = findViewById<TextInputEditText>(R.id.email_editText)
+        val phoneEditTextSignup = findViewById<TextInputEditText>(R.id.phone_editText)
+        val password1EditText = findViewById<TextInputEditText>(R.id.password_1_edittext)
+        val password2EditText = findViewById<TextInputEditText>(R.id.password_2_edittext)
+
+
+
+        password2EditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (password2EditText.text.toString()
+                        .isNotEmpty() && password1EditText.text.toString().isNotEmpty()
+                ) {
+                    if (password2EditText.text.toString()
+                            .equals(password1EditText.text.toString())
+                    ) {
+                        signUpButtonClicked.isEnabled = true
+                    } else
+                        signUpButtonClicked.isEnabled = false
+                } else
+                    signUpButtonClicked.isEnabled = false
+            }
+        })
+
+
+        signUpButtonClicked.setOnClickListener() {
+            Toast.makeText(this, "You are Successfully Signed Up", Toast.LENGTH_SHORT).show()
+            intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
